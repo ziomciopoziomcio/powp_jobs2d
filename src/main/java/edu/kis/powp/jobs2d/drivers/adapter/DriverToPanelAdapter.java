94ee70b257f1d5dafc6ac3af14cb0244af36eda2
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-05 11:26:06 +0100

    Dodano obsługę wbudowanego kontrolera

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverToPanelAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverToPanelAdapter.java
@@ -4,15 +4,17 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class DriverToPanelAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	private final DrawPanelController drawerController;
 
 	public DriverToPanelAdapter() {
-		super();
+		this.drawerController = DrawerFeature.getDrawerController();
 	}
 
 	@Override
@@ -27,7 +29,7 @@ public class DriverToPanelAdapter extends DrawPanelController implements Job2dDr
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		drawerController.drawLine(line);
 	}
 
 	@Override
