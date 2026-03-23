2367e5d0c47b8a4c51c87a9b33faf437b7096298
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-23 11:48:49 +0100

    Usunięty nieużywane extend

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverToPanelAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverToPanelAdapter.java
@@ -9,7 +9,7 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class DriverToPanelAdapter extends DrawPanelController implements Job2dDriver {
+public class DriverToPanelAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
 	private final DrawPanelController drawerController;
 
