0164fee8dd1cc96e350d8411674045c7fc0fa29a
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-03-23 12:00:25 +0100

    Usunięty nieużywany kod

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -58,18 +58,7 @@ public class TestJobs2dPatterns {
 
 		DriverFeature.updateDriverInfo();
 	}
-
-	/**
-	 * Auxiliary routines to enable using Buggy Simulator.
-	 * 
-	 * @param application Application context.
-	 */
-//	private static void setupDefaultDrawerVisibilityManagement(Application application) {
-//		DefaultDrawerFrame defaultDrawerWindow = DefaultDrawerFrame.getDefaultDrawerFrame();
-//		application.addComponentMenuElementWithCheckBox(DrawPanelController.class, "Default Drawer Visibility",
-//			new SelectChangeVisibleOptionListener(defaultDrawerWindow), true);
-//		defaultDrawerWindow.setVisible(true);
-//	}
+	
 
 	/**
 	 * Setup menu for adjusting logging settings.
@@ -97,7 +86,6 @@ public class TestJobs2dPatterns {
 			public void run() {
 				Application app = new Application("2d jobs Visio");
 				DrawerFeature.setupDrawerPlugin(app);
-//				setupDefaultDrawerVisibilityManagement(app);
 
 				DriverFeature.setupDriverPlugin(app);
 				setupDrivers(app);
